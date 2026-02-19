8e4e2754492fa439a9ea50293480f0535baed408
Михайло Олефіренко <79942127+Lemphy@users.noreply.github.com>
2026-02-19 18:37:41 +0200

    Add 8ball fortune-telling script

---
+++ Chatper 7. Arrays/Task 7.13.py
@@ -0,0 +1,22 @@
+import random
+
+def main():
+    infile = open('8ball.txt', 'r', encoding='UTF-8')
+    answers = infile.readlines()
+    infile.close()
+    for i in range(len(answers)):
+        answers[i] = answers[i].rstrip()
+    while True:
+        question = input("Вопрос: ")
+        print(random.choice(answers))
+        print('Продолжаем? (д/н):', end = " ")
+        ans = input()
+        while ans != 'д' and ans != 'н':
+            print("Некорректное значение!")
+            print('Продолжаем? (д/н):', end=" ")
+            ans = input()
+        if ans == 'н':
+            break
+
+if __name__ == "__main__":
+    main()
