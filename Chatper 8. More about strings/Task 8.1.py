ac2542a41708a6e2e866667799f297528b020612
Михайло Олефіренко <79942127+Lemphy@users.noreply.github.com>
2026-02-22 21:33:41 +0200

    Add function to generate initials from full name
    Implement a program to create initials from full name input.

--- Chatper 8. More about strings/Task 8.1.py
+++ Chatper 8. More about strings/Task 8.1.py
@@ -1,1 +1,13 @@
-pass
+def main():
+    name = input("Введите ФИО:")
+    print(create_initials(name))
+
+def create_initials(name):
+    temp = name.split()
+    initials = ''
+    for i in temp:
+        initials += f"{i[0].upper()}."
+    return initials
+
+if __name__ == "__main__":
+    main()
