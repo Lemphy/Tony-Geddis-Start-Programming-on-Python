cd2029589fc93d88899bfc7d947ed5d136a5b811
Михайло Олефіренко <79942127+Lemphy@users.noreply.github.com>
2026-02-22 21:44:44 +0200

    Add functions to calculate sum of digits from input

---
+++ Chatper 8. More about strings/Task 8.2.py
@@ -0,0 +1,17 @@
+def main():
+    val = input("Введите ряд чисел без разделителей: ")
+    print(f"Сумма чисел в строковом значении: {calculate_sum(value = val)}")
+    print(f"Сумма чисел в строковом значении: {calculate_sum_1(value=val)}")
+
+def calculate_sum(value): # 1 вариант
+    temp = 0
+    for i in value:
+        temp += int(i)
+    return temp
+
+def calculate_sum_1(value): #2 вариант
+    temp = [int(i) for i in value]
+    return sum(temp)
+
+if __name__ == "__main__":
+    main()
