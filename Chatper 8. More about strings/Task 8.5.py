f34aa5218a322e758e3e2e78395fefb20c5b9169
Михайло Олефіренко <79942127+Lemphy@users.noreply.github.com>
2026-02-23 18:27:10 +0200

    Implement phone number conversion from letters to digits
    Add a function to convert phone numbers from letters to digits.

---
+++ Chatper 8. More about strings/Task 8.5.py
@@ -0,0 +1,24 @@
+def main():
+    lst_1 = [2,3,4,5,6,7,8,9]
+    lst_2 = ['ABC','DEF','GHI','JKL','MNO','PQRS','TUV','WXYZ']
+    response = input('Введите 10-символьный номер телефона в формате XXX-XXX-XXXX: ')
+    response_lst = response.split('-')
+    print(f"Вы ввели: {response_lst}")
+    temp = ""
+    counter = 0
+    for i in range(len(response_lst)): # проходим введенный пользователем список
+        for j in response_lst[i].upper(): # заходим в строковое значение елемента списка, j проверяет каждую букву i элемента
+            if j.isalpha(): # если мы видим что значение это буква
+                for x in range(len(lst_2)): # проходим готовый список буквенных символов
+                    check = lst_2[x].find(j) # если находим символ в x элементе
+                    if check != -1: # если метод find() нашел значение индекса элемента j
+                        temp += f"{lst_1[x]}"  # присваиваем значение цифры относящееся к этой букве
+            else:
+                temp += f"{j}"
+        if counter < 2: # печатаем разделители когда заканчиваем проверять i значение списка
+            counter += 1
+            temp += f"-"
+    print(temp)
+
+if __name__ == "__main__":
+    main()
