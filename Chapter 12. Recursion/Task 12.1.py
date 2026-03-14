e478cccb8b4af3753ce006efd001704e0515c0bc
Михайло Олефіренко <79942127+Lemphy@users.noreply.github.com>
2026-03-14 14:03:41 +0200

    Add recursive printing function
    Implement recursive function to print numbers in order.

--- Chapter 12. Recursion/Task 12.1.py
+++ Chapter 12. Recursion/Task 12.1.py
@@ -1,1 +1,14 @@
+# 12.1 Рекурсивная печать
 
+def main():
+    recursion(int(input("Введите число: ")))
+
+def recursion(n: int):
+    if n < 1: # базовый случай
+        return
+    else: # рекурсивный случай
+        recursion(n - 1)
+        print(n)
+
+if __name__ == "__main__":
+    main()
