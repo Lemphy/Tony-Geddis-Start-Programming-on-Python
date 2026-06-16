c6c31b3e2b43def3d4266a777c44416d3fcc6872
Михайло Олефіренко <79942127+Lemphy@users.noreply.github.com>
2026-06-16 16:37:28 +0300

    Add function to convert kilometers to miles

---
+++ Chapter 5. Functions/Task 5.1.py
@@ -0,0 +1,11 @@
+MIL = 0.6214
+
+def calc_mil(arg1):
+    return arg1 * MIL
+
+def main():
+    distance = int(input('Введите расстояние в километрах: '))
+    print(f'Расстояние в милях: {calc_mil(distance):,.2f}')
+
+if __name__ == '__main__':
+    main()
