d8931e1e96a95219213a21dafae095b30198df17
Михайло Олефіренко <79942127+Lemphy@users.noreply.github.com>
2026-06-17 18:45:12 +0300

    Create Task 6.8.py

---
+++ Chapter 6. Files and exceptions/Task 6.8.py
@@ -0,0 +1,29 @@
+# 6.7 Программа чтения файла со случайными числами
+import random
+
+def create_file_with_values(counter):
+    # Создаем файл с числами
+    outfile = open('numbers.txt', 'w', encoding='UTF-8')
+    for i in range(counter):
+        outfile.write(f'{random.randint(1,500)}\n')
+    outfile.close()
+
+def main():
+    counter = int(input('Введите количество желаемых рандомных значений в файле: '))
+    create_file_with_values(counter)
+    outfile = open('numbers.txt', 'r', encoding='UTF-8')
+    total = 0
+    counter = 0
+    for number in outfile:
+        number = int(number.rstrip())
+        total += number
+        counter += 1
+        print(number, end = ' ')
+    print()
+    outfile.close()
+    print(f'Сумма чисел: {total}\n'
+          f'Количество случайных чисел, прочитанных из файла: {counter}\n'
+          f'Среднее значение: {total/counter}')
+
+if __name__ == '__main__':
+    main()
